aca2e6b3128999aa3043c7f5bb281c5b2622943e
madyanes <madyanes.dev@gmail.com>
2023-08-06 16:43:28 +0700

    cleanup event listener

--- src/Counter.jsx
+++ src/Counter.jsx
@@ -3,10 +3,21 @@ import React from 'react'
 const Counter = () => {
   const [count, setCount] = React.useState(0)
 
+  const logSomething = () => {
+    console.log('something')
+  }
+
   // menggantikan componentDidMount dan componenDidUpdate
   React.useEffect(() => {
     console.count('di dalam useEffect')
     document.title = `You've clicked ${count}x`
+
+    const btn = document.getElementsByTagName('button')[0]
+    btn.addEventListener('click', logSomething)
+
+    return () => {  // tanpa kode ini (return untuk cleanup), event listener akan selalu bertambah setiap kali komponen di-render
+      btn.removeEventListener('click', logSomething)
+    }
   })
 
   const increase = () => setCount((prevCount) => prevCount + 1)
